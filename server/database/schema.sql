57529d581e9330bfb714fd5a2e78d4517bb8ec58
Aurélien Lavanchy <lavanchy.aurelien@gmail.com>
2024-06-11 14:26:57 +0200

    feat(schema.sql): initial setup of the tables

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,12 +1,64 @@
-create table user (
-  id int unsigned primary key auto_increment not null,
-  email varchar(255) not null unique,
-  password varchar(255) not null
+-- Vidéo
+CREATE TABLE video (
+  id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  name VARCHAR(80) NOT NULL,
+  description TEXT,
+  date DATE NOT NULL,
+  is_premium BOOLEAN NOT NULL,
+  is_free BOOLEAN NOT NULL,
+  requires_account BOOLEAN NOT NULL
 );
 
-create table item (
-  id int unsigned primary key auto_increment not null,
-  title varchar(255) not null,
-  user_id int unsigned not null,
-  foreign key(user_id) references user(id)
+-- Catégorie
+CREATE TABLE category (
+  id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  name VARCHAR(80) NOT NULL
+);
+
+CREATE TABLE sub_category (
+  id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  name VARCHAR(80) NOT NULL,
+  category_id INT UNSIGNED,
+  FOREIGN KEY(category_id) REFERENCES category(id)
+);
+
+-- Utilisateur
+CREATE TABLE role (
+  id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  name VARCHAR(80) NOT NULL,
+  access VARCHAR(40) NOT NULL
+);
+
+CREATE TABLE user (
+  id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  firstname VARCHAR(120) NOT NULL,
+  lastname VARCHAR(120) NOT NULL,
+  email VARCHAR(120) NOT NULL,
+  password TEXT NOT NULL,
+  birthdate DATE,
+  role_id INT UNSIGNED,
+  FOREIGN KEY(role_id) REFERENCES role(id)
+);
+
+-- Table de jointure
+CREATE TABLE video_category (
+  video_id INT UNSIGNED,
+  sub_category_id INT UNSIGNED,
+  FOREIGN KEY(video_id) REFERENCES video(id),
+  FOREIGN KEY(sub_category_id) REFERENCES sub_category(id)
+);
+
+CREATE TABLE payment (
+  id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  date DATE NOT NULL,
+  type VARCHAR(40) NOT NULL,
+  user_id INT UNSIGNED,
+  FOREIGN KEY(user_id) REFERENCES user(id)
+);
+
+CREATE TABLE privilege (
+  video_id INT UNSIGNED,
+  user_id INT UNSIGNED,
+  FOREIGN KEY(video_id) REFERENCES video(id),
+  FOREIGN KEY(user_id) REFERENCES user(id)
 );

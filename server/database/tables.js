ec50096b5a1d1dc1a9374f66717495761eebf9f7
camille-haxa <camille.haxa@proton.me>
2024-06-12 11:27:23 +0200

    fix: add video repository to tables

--- server/database/tables.js
+++ server/database/tables.js
@@ -1,5 +1,6 @@
 // Import the repository modules responsible for handling data operations on the tables
 const ItemRepository = require("./models/ItemRepository");
+const VideoRepository = require("./models/VideoRepository");
 
 // Create an empty object to hold data repositories for different tables
 const tables = {};
@@ -11,6 +12,7 @@ const tables = {};
 // Register each repository as data access point for its table
 tables.item = new ItemRepository();
 
+tables.video = new VideoRepository();
 /* ************************************************************************* */
 
 // Use a Proxy to customize error messages when trying to access a non-existing table

c97874a364c8803223f1e1fc4cd2ddfdf2531123
JohaneDecamps <johane.decamps@gmail.com>
2024-06-12 11:01:30 +0200

    feat : category route

---
+++ server/app/routers/api/categories/router.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const router = express.Router();
+
+const { browse, read, edit, add, destroy } = require("../../../controllers/caterogyActions");
+
+router.get("/", browse); 
+
+router.get("/:id", read); 
+
+router.put("/:id", edit);
+
+router.post("/", add);
+
+router.destroy("/:id", destroy)
+
+module.exports = router;
---
+++ server/database/models/CategoryRepository.js
@@ -0,0 +1,46 @@
+const AbstractRepository = require("./AbstractRepository"); 
+
+class CategoryRepository extends AbstractRepository{
+constructor() {
+    super({table : "category"});
+}
+
+async browse(){
+    const [result] = await this.database.query(`select name from ${this.table}`); 
+
+    return result; 
+}
+
+async read (id) {
+    const [rows] = await this.database.query(`select name from ${this.table} where id = ?`, [id]);
+
+    return rows[0]; 
+}
+
+async add (category) {
+    const [rows] = await this.database.query(
+        `insert into ${this.table} (name) value = ?`, 
+        [category.name]
+        );
+
+    return rows.insertId;
+}
+
+
+async update(category) {
+    const [result] = await this.database.query(`update ${this.table} set name = ? where id = ? `, 
+[category.name, category.id])
+
+return result.affectedRows;
+}
+
+
+async destroy (id) {
+    const [row] = await this.database.query(
+        `delete from ${this.table} where id = ?`,
+        [id]
+    );
+}
+}
+
+module.exports = CategoryRepository

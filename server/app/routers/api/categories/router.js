196c288701b9bb9e0586afd8712bd3ab3ac9f01d
Aurélien Lavanchy <lavanchy.aurelien@gmail.com>
2024-06-13 12:02:03 +0200

    fix: wrong name for categoryActions

--- server/app/routers/api/categories/router.js
+++ server/app/routers/api/categories/router.js
@@ -2,16 +2,22 @@ const express = require("express");
 
 const router = express.Router();
 
-const { browse, read, edit, add, destroy } = require("../../../controllers/caterogyActions");
+const {
+  browse,
+  read,
+  edit,
+  add,
+  destroy,
+} = require("../../../controllers/categoryActions");
 
-router.get("/", browse); 
+router.get("/", browse);
 
-router.get("/:id", read); 
+router.get("/:id", read);
 
 router.put("/:id", edit);
 
 router.post("/", add);
 
-router.destroy("/:id", destroy)
+router.destroy("/:id", destroy);
 
 module.exports = router;

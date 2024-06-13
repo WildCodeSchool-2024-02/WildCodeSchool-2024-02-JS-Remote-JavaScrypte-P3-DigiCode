257e8f3c0be79ae9c26ea40deb0c54ba949164cd
MichaelLELU <lelu.michael@gmail.com>
2024-06-13 10:53:56 +0200

    feat: initial frontend

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App";
+import HomePage from "./pages/homepage/HomePage";
+import CategoryPage from "./pages/categorypage/CategoryPage";
+import VideoPage from "./pages/videopage/VideoPage";
+
 
 const router = createBrowserRouter([
   {
-    path: "/",
     element: <App />,
+    children: [
+      {
+         path: "/",
+         element: <HomePage />
+      },
+      {
+        path: "/category",
+        element: <CategoryPage />
+      },
+      {
+       path : "/video/:id",
+       element : <VideoPage /> 
+      },
+    ]
   },
 ]);
 

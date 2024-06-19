e25322ee38dfa84f0b18c53acf9c5fae9f2a450c
camille-haxa <camille.haxa@proton.me>
2024-06-19 10:25:10 +0200

    feat: create video seeder, delete item seeder

---
+++ server/database/fixtures/VideoSeeder.js
@@ -0,0 +1,28 @@
+const AbstractSeeder = require("./AbstractSeeder");
+
+class VideoSeeder extends AbstractSeeder {
+  constructor() {
+    // call the constructor of the parent class (AbstractSeeder) with appropriate options
+    super({ table: "video", truncate: true });
+  }
+
+  // populate the "video" table with fake data
+  run() {
+    // generate and insert fake data into the "video" table
+    for (let i = 0; i < 10; i += 1) {
+      // generate fake video data
+      const fakeVideo = {
+        name: this.faker.lorem.words(), // generates a fake name (title)
+        url: this.faker.internet.url(), // generates a fake url
+        image: this.faker.image.urlLoremFlickr(), // generates a fake image
+        description: this.faker.lorem.paragraph(), // generates a fake description
+        date: this.faker.date.recent(), // generate a fake date
+      };
+
+      // insert the fakeVideo data into the "video" table
+      this.insert(fakeVideo);
+    }
+  }
+}
+
+module.exports = VideoSeeder;

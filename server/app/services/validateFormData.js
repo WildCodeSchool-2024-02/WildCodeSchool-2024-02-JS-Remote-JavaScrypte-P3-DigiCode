27322d9a2852fbc938bf3a524c9f98faecf02462
Aurélien Lavanchy <lavanchy.aurelien@gmail.com>
2024-06-20 10:56:52 +0200

    fix(validateFormData): .string is a function

--- server/app/services/validateFormData.js
+++ server/app/services/validateFormData.js
@@ -1,13 +1,13 @@
 const Joi = require("joi");
 
 const formDataSchema = Joi.object({
-  firstname: Joi.string.max(120).required(),
-  lastname: Joi.string.max(120).required(),
-  email: Joi.string
+  firstname: Joi.string().max(120).required(),
+  lastname: Joi.string().max(120).required(),
+  email: Joi.string()
     .max(120)
     .regex(/^((?!\.)[\w\-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/)
     .required(),
-  password: Joi.string
+  password: Joi.string()
     .max(64)
     .regex(/^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\w\d\s:])([^\s]){16,64}$/)
     .required(),

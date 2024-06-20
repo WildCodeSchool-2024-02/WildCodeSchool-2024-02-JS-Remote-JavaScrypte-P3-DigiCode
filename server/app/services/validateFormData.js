7d36da172f82997559ad1e78952a1ee6dd26863b
Aurélien Lavanchy <lavanchy.aurelien@gmail.com>
2024-06-20 10:19:52 +0200

    fix(validateFormData): missing regex pattern for email and password

--- server/app/services/validateFormData.js
+++ server/app/services/validateFormData.js
@@ -3,8 +3,14 @@ const Joi = require("joi");
 const formDataSchema = Joi.object({
   firstname: Joi.string.max(120).required(),
   lastname: Joi.string.max(120).required(),
-  email: Joi.string.max(120).required(),
-  password: Joi.string.max(64).required(),
+  email: Joi.string
+    .max(120)
+    .regex(/^((?!\.)[\w\-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/)
+    .required(),
+  password: Joi.string
+    .max(64)
+    .regex(/^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\w\d\s:])([^\s]){16,64}$/)
+    .required(),
 });
 
 const validateFormData = (req, res, next) => {

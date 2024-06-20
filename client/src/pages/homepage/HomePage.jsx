a3f5b9ea28e9572d5ed1d7d626f2435cddc2e2bb
camille-haxa <camille.haxa@proton.me>
2024-06-20 17:41:18 +0200

    fix: add media queries to slider

--- client/src/pages/homepage/HomePage.jsx
+++ client/src/pages/homepage/HomePage.jsx
@@ -15,17 +15,34 @@ export default function HomePage() {
   return (
     <>
       <Swiper
+        slidesPerView={1}
+        spaceBetween={50}
+        breakpoints={{
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+          },
+        }}
         pagination={{
           clickable: true,
         }}
+        style={{
+          "--swiper-navigation-color": "rgb(255,250,250)",
+          "--swiper-pagination-color": "rgb(255,250,250)",
+        }}
         navigation
         modules={[Pagination, Navigation]}
-        className="mySwiper"
+        className="heroSwiper"
+        id="heroSwiper"
       >
-        {videoData.slice(0, 3).map((v) => (
-          <SwiperSlide key={v.name}>
+        {videoData.slice(0, 5).map((v) => (
+          <SwiperSlide key={v.name} id="heroSlide">
             {" "}
-            <img src={v.image} alt={v.alt} />
+            <img id="imageSlider" src={v.image} alt={v.alt} />
           </SwiperSlide>
         ))}
       </Swiper>

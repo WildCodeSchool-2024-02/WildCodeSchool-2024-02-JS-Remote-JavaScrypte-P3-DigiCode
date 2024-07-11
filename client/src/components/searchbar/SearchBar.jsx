c8788cbba17bf31362ff89084adf6671bbd50b73
MichaelLELU <lelu.michael@gmail.com>
2024-07-11 09:53:18 +0200

    fix: navbar

--- client/src/components/searchbar/SearchBar.jsx
+++ client/src/components/searchbar/SearchBar.jsx
@@ -1,18 +1,11 @@
-/* eslint-disable react/jsx-props-no-spreading */
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useForm } from "react-hook-form";
 
 import "./SearchBar.css";
 
 export default function SearchBar() {
   const [inputValue, setInputValue] = useState(null);
   const navigate = useNavigate();
-
-  const {
-    register,
-    formState: { errors },
-  } = useForm();
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -27,19 +20,11 @@ export default function SearchBar() {
         id="search"
         type="text"
         placeholder="Search"
-        {...register("search", {
-          required: "This filed is required !",
-          minLength: {
-            value: 2,
-            message: "You need at least 2 characters",
-          },
-        })}
         onChange={(event) => setInputValue(event.target.value)}
       />
       <button className="searchButton" type="submit">
         Search
       </button>
-      {errors.search && <p className="form-error">{errors.search.message}</p>}
     </form>
   );
 }

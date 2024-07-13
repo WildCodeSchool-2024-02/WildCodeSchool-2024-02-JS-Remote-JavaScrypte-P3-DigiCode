969fad23936938ae0093097a0923dd7beb0d939a
Aurélien Lavanchy <lavanchy.aurelien@gmail.com>
2024-07-13 16:31:50 +0200

    fix: removed unnecessary css file

--- client/src/components/videopanel/VideoPanel.jsx
+++ client/src/components/videopanel/VideoPanel.jsx
@@ -2,7 +2,6 @@
 import axios from "axios";
 import { useForm } from "react-hook-form";
 import { useEffect, useState } from "react";
-import "./VideoPanel.css";
 
 export default function VideoPanel() {
   const [categoryData, setCategoryData] = useState();

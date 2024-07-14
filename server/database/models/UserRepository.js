5ab164bd5670fc688081cf8e0de8b5a99c1ee8b4
Aurélien Lavanchy <lavanchy.aurelien@gmail.com>
2024-07-14 15:14:38 +0200

    fix(UserRepository): removed duplicated entry

--- server/database/models/UserRepository.js
+++ server/database/models/UserRepository.js
@@ -18,7 +18,7 @@ class UserRepository extends AbstractRepository {
   // Read
   async readById(id) {
     const [row] = await this.database.query(
-      `SELECT u.firstname, u.lastname, u.email, u.password, r.name AS role_name, r.name AS role FROM ${this.table} AS u JOIN role AS r ON u.role_id=r.id WHERE u.id=?`,
+      `SELECT u.firstname, u.lastname, u.email, u.password, r.name AS role FROM ${this.table} AS u JOIN role AS r ON u.role_id=r.id WHERE u.id=?`,
       [id]
     );
 

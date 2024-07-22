6427cc702d65eca1fc9309a2745b9bd6e173882e
MichaelLELU <lelu.michael@gmail.com>
2024-07-22 12:15:02 +0200

    fix: redirection isnotconnected message

--- client/src/components/videocard/VideoCard.jsx
+++ client/src/components/videocard/VideoCard.jsx
@@ -1,14 +1,16 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useOutletContext } from "react-router-dom";
 import PropTypes from "prop-types";
 import "./VideoCard.css";
 
 export default function VideoCard({ video }) {
+  const { currentUser } = useOutletContext();
+
   return (
     <NavLink className="cardContainer" to={`/video/${video.id}`}>
       <div className="overlay">
         <h2 className="cardTitle">{video.title}</h2>
       </div>
-      {video.is_connected === 0 ? (
+      {video.is_connected === 0 || currentUser != null ? (
         <img className="cardImage" src={video.image} alt={video.title} />
       ) : (
         <img id="imageNotConnected" src={video.image} alt={video.title} />
--- client/src/pages/videopage/VideoPage.jsx
+++ client/src/pages/videopage/VideoPage.jsx
@@ -4,6 +4,7 @@ import {
   useNavigate,
   useOutletContext,
 } from "react-router-dom";
+import { useEffect } from "react";
 import "./VideoPage.css";
 
 export default function VideoPage() {
@@ -12,39 +13,54 @@ export default function VideoPage() {
 
   const Navigate = useNavigate();
 
+  useEffect(() => {
+    if (videoData.is_connected && !currentUser) {
+      setTimeout(() => Navigate("/login"), 5000);
+    }
+  }, [videoData.is_connected, currentUser, Navigate]);
+
   const handleBack = () => {
     window.history.back();
   };
 
-  return videoData.is_connected && currentUser == null ? (
-    setTimeout(() => Navigate("/login"), 3000)
-  ) : (
-    <div className="video-container">
+  return (
+    <>
       <button type="button" onClick={handleBack} className="backButton">
         Back
       </button>
-      <h1 className="video-title">{videoData.title}</h1>
-      <video controls poster={videoData.image} className="video-playback">
-        <source src={videoData.url} type="video/mp4" />
-        <track kind="captions" />
-      </video>
-      <div className="video-description">
-        <p className="video-metadata">
-          <span>
-            {videoData.date != null ? videoData.date.slice(0, 10) : "No date"}
-          </span>{" "}
-          <span>
-            {videoData.category != null ? (
-              <Link to={`/categories/${videoData.category}`}>
-                {videoData.category}
-              </Link>
-            ) : (
-              "No category"
-            )}
-          </span>
-        </p>
-        <p>{videoData.description}</p>
-      </div>
-    </div>
+      {videoData.is_connected && currentUser === null ? (
+        <h1 className="video-title">
+          To view this video, you need to be connected. You will be redirected
+          to the login page . . .
+        </h1>
+      ) : (
+        <div className="video-container">
+          <h1 className="video-title">{videoData.title}</h1>
+          <video controls poster={videoData.image} className="video-playback">
+            <source src={videoData.url} type="video/mp4" />
+            <track kind="captions" />
+          </video>
+          <div className="video-description">
+            <p className="video-metadata">
+              <span>
+                {videoData.date != null
+                  ? videoData.date.slice(0, 10)
+                  : "No date"}
+              </span>{" "}
+              <span>
+                {videoData.category != null ? (
+                  <Link to={`/categories/${videoData.category}`}>
+                    {videoData.category}
+                  </Link>
+                ) : (
+                  "No category"
+                )}
+              </span>
+            </p>
+            <p>{videoData.description}</p>
+          </div>
+        </div>
+      )}
+    </>
   );
 }

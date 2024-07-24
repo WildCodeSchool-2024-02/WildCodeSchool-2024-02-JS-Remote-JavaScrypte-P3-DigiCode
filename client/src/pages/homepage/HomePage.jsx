4dbc11eb5c9953e8a6c89f16905329938d301b4f
MichaelLELU <lelu.michael@gmail.com>
2024-07-24 10:03:32 +0200

    feat: randomvideolist and fix css homepage

--- client/src/pages/homepage/HomePage.jsx
+++ client/src/pages/homepage/HomePage.jsx
@@ -1,6 +1,6 @@
 import { useLoaderData } from "react-router-dom";
 
-import { BookMarkedIcon, SparklesIcon } from "lucide-react";
+import { BookMarkedIcon, SparklesIcon, Dices } from "lucide-react";
 import HeroSlider from "../../components/HeroSlider/HeroSlider";
 import SearchBar from "../../components/searchbar/SearchBar";
 import VideoList from "../../components/videolist/VideoList";
@@ -11,6 +11,8 @@ export default function HomePage() {
   const videoData = useLoaderData();
 
   const newVideos = videoData.sort((a, b) => b.id - a.id).slice(0, 9);
+  const randommedVideos = videoData.sort(() => Math.random() - 0.5);
+  const randomVideos = randommedVideos.slice(0, 9);
 
   return (
     <>
@@ -21,11 +23,16 @@ export default function HomePage() {
 
       <SearchBar />
 
-      <h2>
+      <h2 className="title-home">
         {"New videos "}
         <SparklesIcon color="#FFDF00" strokeWidth={1.75} />
       </h2>
       <VideoList videoData={newVideos} />
+      <h2 className="title-home">
+        {"Random videos "}
+        <Dices color="#1FD360" strokeWidth={1.75} />
+      </h2>
+      <VideoList videoData={randomVideos} />
     </>
   );
 }
